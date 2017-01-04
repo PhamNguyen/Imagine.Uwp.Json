ca101e8d6f229571c4c78761950d11081cc673b5
PhamNguyen <pham.nguyen@hotmail.com>
2017-01-04 18:04:45 +0700

    AssemblyInfo

--- JsonUWP/Properties/AssemblyInfo.cs
+++ JsonUWP/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("JsonUWP")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Json Parse for Universal Windows Platform")]
+[assembly: AssemblyDescription("Json Parse for Universal Windows Platform")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("JsonUWP")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCompany("Imagine Studio")]
+[assembly: AssemblyProduct("Json Parse for Universal Windows Platform")]
+[assembly: AssemblyCopyright("Copyright © 2017 by Imagine Studio")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
